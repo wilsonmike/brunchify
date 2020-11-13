9243a517017de18e666a165ec4eaa866f23575a9
mike <michaelwilsonsj@hotmail.com>
2020-11-13 17:04:47 -0500

    setting up bare bones/ added header and form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FavoritesPageComponent } from './favorites-page/favorites-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'recipe-list', component: RecipeListComponent },
+  { path: 'favorites-page', component: FavoritesPageComponent },
+  { path: '', redirectTo: '/recipe-list', pathMatch: 'full' }, // redirect to `first-component`
+  { path: '**', component: PageNotFoundComponent }, // Wildcard route for a 404 page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchCriteriaComponent } from './search-criteria/search-criteria.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { FavoritesPageComponent } from './favorites-page/favorites-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchCriteriaComponent,
+    RecipeListComponent,
+    FavoritesPageComponent,
+    PageNotFoundComponent,
+    HeaderComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
