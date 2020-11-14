92d8f6fe8587460ace06e68682bdfa06d2d80c38
mike <michaelwilsonsj@hotmail.com>
2020-11-14 18:37:42 -0500

    started search term functionality | displayed recipe images and set grid for resposivensess

--- src/app/recipe.service.ts
+++ src/app/recipe.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { API_KEY, APP_ID } from '../keys';
+import { API_KEY, APP_ID } from './keys';
 import { Observable } from 'rxjs';
 import { Recipe, Hit, RecipeResponse } from './interfaces/recipe';
 @Injectable({
